b3a8df8f23b91399aa1d49ecae595a6ac03d9f35
Walter Montes <walmon93@hotmail.com>
2018-08-12 10:57:20 -0600

    use env vars

--- @worldsibu/server/src/utils/helper.ts
+++ @worldsibu/server/src/utils/helper.ts
@@ -1,8 +1,8 @@
 export class Helper {
   /** Ref to the CC name */
-  public static get drugCC(): string { return 'drugChainCode'; }
+  public static get drugCC(): string { return process.env.CHAINCODE; }
   /** Ref to the preferred channel name.
    * In real life application this may be dynamic.
    */
-  public static get channel(): string { return 'ch1'; }
+  public static get channel(): string { return process.env.CHANNEL; }
 }
